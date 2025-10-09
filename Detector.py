497275ee0867b9ab69390f1410b8f626983c6e37
Shank <shashankcactus@gmail.com>
2025-10-08 23:31:01 -0400

    Update Detector.py
    Made more functions so its a bit easier to read, and we can explain what each function does in great detail during our presentation.

--- Detector.py
+++ Detector.py
@@ -1,91 +1,91 @@
 from Bio import SeqIO
 from Bio.Align import PairwiseAligner
-from Bio.Seq import Seq
-from Bio.Blast import NCBIWWW, NCBIXML
-import urllib.request, time, gzip
-from io import StringIO 
-import numpy as np
+import urllib.request, gzip, numpy as np
 
 
 percentage_list = []
 
 
 #Stores percentages, updates the average, and writes the results down in the md file
+
+# === percentage list and average ===
 def add_to_list(percentage, out_handle, final = False):
 
     if not final:
         percentage_list.append(round(percentage, 3)) #Rounds to 3 decimals places
-
-        #Writes this in the md file 
         out_handle.write(f"This animal is {percentage:.2f}% human!\n") 
 
         if percentage >= 95.00:
             out_handle.write(f"This animal is human!\n \n")
+
         else:
             out_handle.write(f"This animal is not human!\n \n")
+
     else:
         percent_average = np.mean(percentage_list)
-        out_handle.write(f"\nOn average, this animal is {percent_average:.2f}% human!\n") 
+        out_handle.write(f"\nOn average, this animal is {percent_average:.2f}% human!\n") #Why is this guy not writing :rage:
 
 
+# === user file ===
+def read_input_file(filename):
+    return SeqIO.read(filename, "fasta")
 
 
-def human_percentage_blast():
-    examplefasta = "chimp.fasta"  #File that is being used to compare to human. Currently its being used to compare and search for itself.
+# === human genome file ===
+def read_human_fasta(humanfasta):
+        with urllib.request.urlopen(humanfasta ) as human:
+            with gzip.open(human, "rt") as fasta:
+                 for humanF in SeqIO.parse(fasta, "fasta"):
+                     yield humanF
+
+
+# === alignment and percentage calculator ===
+def alignment_calculator(output_seq, example_seq):
+    #Alignment 
+    aligner = PairwiseAligner()
+    aligner.mode = "local"
+    aligner.match_score = 1
+    aligner.mismatch_score = 0
+    aligner.open_gap_score = 0
+    aligner.extend_gap_score = 0
+                    
+                    
+    #Score calculator 
+    matches = 0
+    bases = 0
+    score = aligner.score(str(output_seq[:100000]), str(example_seq))
+    matches += score
+    bases += len(example_seq)
+    percentage = (matches / bases) * 100
+    return percentage
+
+
+    
+# === main === 
+def main():
+    examplefasta = "chimp.fasta" #File that is being used to compare to human. Currently its being used to compare and search for itself.
 
     humanfasta = "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz" #Human genome 3.1GB file
     
     outputfile = "blastresult.md" #All the results will get thrown into here, is markdown(md); light weight.
 
-    example = SeqIO.read(examplefasta, "fasta") 
+    example = read_input_file(examplefasta) #Reads the example fasta file
 
-    with open(outputfile, "w") as out_handle: #As you are writing in the outputfile:
-        with urllib.request.urlopen(humanfasta) as huma:
-            with gzip.open(huma, "rt") as human:
-                for i, hum in enumerate(SeqIO.parse(human, "fasta")):
-                    out_handle.write(f"{i+1}, ID: {hum.id}, SeqRecord: {len(hum.seq)}\n")
+    with open(outputfile, "w") as out_handle:
+        for i, output in enumerate(read_human_fasta(humanfasta)):
+            out_handle.write(f"{i+1}, ID: {output.id}, SeqRecord: {len(output.seq)}\n")
 
-
-                    #Alignment 
-                    aligner = PairwiseAligner()
-                    aligner.mode = "local"
-                    aligner.match_score = 1
-                    aligner.mismatch_score = 0
-                    aligner.open_gap_score = 0
-                    aligner.extend_gap_score = 0
+            percentage = alignment_calculator(output.seq, example.seq) #Calculates the percentage of human DNA in the example file
+            add_to_list(percentage, out_handle) #Adds the percentage to the list and writes it to the md file
 
 
-                    #Score calculator
-                    matches = 0
+            #So we don't brick our pc :D
+            if i >= 5: #For some reason always does 2 more than this number, I don't know why. 
+                break
+            
+        add_to_list(0, out_handle, final = True) #Final average calculation             
 
-                    bases = 0
+main() #Prints the class
 
-                    score = aligner.score(str(hum.seq[:100000]), str(example.seq))
-
-                    matches += score
-
-                    bases += len(example.seq)
-
-                    percentage = (matches / bases) * 100
-
-
-                    add_to_list(percentage, out_handle)
-
-
-                    #So we don't brick our pc :D
-                    if i >= 7:
-                        break
-        add_to_list(0, out_handle, final=True) #Final average calculation             
-
-human_percentage_blast() #Prints the class
-
-
-
-
-
-
-
-
-    
-
-
+if __name__ == "__main__":
+    main()
